a81d60d726c21c00bb69f5f18ae11bbb49c82bd5
Fraz Aslam <77319791+frazaslam99@users.noreply.github.com>
2021-03-17 11:56:03 +0500

    Covid React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from "./Component/Navbar"
+import Info from "./Component/info"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Info/>
+
+    </div>
+  );
+}
+
+export default App;
