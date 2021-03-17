dec3e5057b5254574c5960cfbe2b4c5349adb1ea
Fraz Aslam <77319791+frazaslam99@users.noreply.github.com>
2021-03-17 18:49:49 +0500

    Covid App Complete

--- src/Component/Globalstate.js
+++ src/Component/Globalstate.js
@@ -2,7 +2,7 @@ import React,{useEffect,useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Autorenew } from '@material-ui/icons';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,9 +1,8 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import {makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
